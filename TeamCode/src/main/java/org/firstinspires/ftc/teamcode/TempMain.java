a8dffb1ad344f29c1033f38084cfba183222d17a
Theo Marshall <theo@seelye.net>
2019-10-22 16:56:24 -0700

    added inverse kinematics and initial unstow behavior

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TempMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TempMain.java
@@ -6,11 +6,13 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="temp", group="Iterative Opmode")
 @Disabled
 public class TempMain extends OpMode {
+
+    //uses positional IK control if true, uses angle based control if false.
+    private boolean positionalControl = false;
 
     //degrees arm movement per encoder tick (incl gears between motors and arm)
     private final double LOWERARM_MOTOR_DEG_PER_TICK = 360.0 / 3892;
@@ -22,21 +24,19 @@ public class TempMain extends OpMode {
     private final double MAXREACH = LOWERARM_LENGTH + UPPERARM_LENGTH;
     private final double MINREACH = LOWERARM_LENGTH - UPPERARM_LENGTH;
 
+    private final double INITIAL_LOWERANGLE = 40.0;
+    private final double INITIAL_UPPERANGLE = -130.0;
+
     private enum ArmMode {
-        STOPPED, RUNNING, CANNOT_REACH;
+        STOPPED, RUNNING, CANNOT_REACH, INIT_UNSTOW, DUCK
     }
-    /*
-    private final int ARM_STOPPED = 0;
-    private final int ARM_RUNNING = 1;
-    private final int ARM_CANNOT_REACH = 2;
-    */
 
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor lowerMotor = null;
     private DcMotor upperMotor1 = null;
     private DcMotor upperMotor2 = null;
 
-    ArmMode armStatus = ArmMode.STOPPED;
+    ArmMode armStatus = ArmMode.INIT_UNSTOW;
 
     /*
      * Code to run ONCE when the driver hits INIT
@@ -81,12 +81,11 @@ public class TempMain extends OpMode {
         upperMotor2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
     }
 
-    private double targetX = 0.0;
-    private double targetY = MINREACH;
+    private double targetX = 24.0;
+    private double targetY = 10.0;
 
-    //todo: temp run to angle vars, replace w/ positional control
-    private double lowerTargetAngle = 0.0;
-    private double upperTargetAngle = 0.0;
+    private double lowerTargetAngle = 40.0;
+    private double upperTargetAngle = -130.0;
 
     /*
      * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
@@ -102,9 +101,14 @@ public class TempMain extends OpMode {
             //maybe todo: find a way like motor mode to fully stop?
         }
 
-        //change target by left stick
-        lowerTargetAngle += gamepad1.left_stick_y;
-        upperTargetAngle += gamepad1.right_stick_y;
+        if(positionalControl) {
+            targetX += gamepad1.left_stick_x;
+            targetY += gamepad1.left_stick_y;
+        } else {
+            //change target by left stick
+            lowerTargetAngle += gamepad1.left_stick_y;
+            upperTargetAngle += gamepad1.right_stick_y;
+        }
 
         double targetDist = Math.sqrt(targetX * targetX + targetY * targetY);
         if(targetDist > MAXREACH || targetDist < MINREACH) {
@@ -119,10 +123,30 @@ public class TempMain extends OpMode {
             lowerMotor.setTargetPosition(lowerMotor.getCurrentPosition());
             upperMotor1.setTargetPosition(upperMotor1.getCurrentPosition());
             upperMotor2.setTargetPosition(upperMotor2.getCurrentPosition());
+        } else if(armStatus == ArmMode.INIT_UNSTOW) {
+            if(runtime.seconds() < 2.5) {
+                lowerMotor.setTargetPosition((int) Math.round((90.0 - INITIAL_LOWERANGLE) / LOWERARM_MOTOR_DEG_PER_TICK));
+            } else {
+                armStatus = ArmMode.STOPPED;
+            }
         } else if(armStatus == ArmMode.RUNNING) {
             //todo: inverse kinematics code here!
             //double angle1 = Math.acos(()/(-2*))
+            if(positionalControl) {
+                //the angle between a direct line to the target point from the base of the arm
+                double D1 = Math.atan2(targetY, targetX);
+                //the angle between the lower arm section and D1
+                double D2 = lawOfCosines(targetDist, LOWERARM_LENGTH, UPPERARM_LENGTH);
+                //the absolute angle of the lower arm section related to the horizontal axis of the robot
+                double lowerSectionAngle = D1 + D2;
+                //the internal angle between the arm segments
+                double internalAngle = lawOfCosines(LOWERARM_LENGTH, UPPERARM_LENGTH, targetDist);
+                //the absolute angle of the upper arm section related to the horizontal axis of the robot
+                double upperAbsoluteAngle = lowerSectionAngle - (Math.PI - internalAngle);
 
+                lowerTargetAngle = lowerSectionAngle;
+                upperTargetAngle = upperAbsoluteAngle;
+            }
 
             //run to angle
             int lowerTargetTicks = (int) Math.round(lowerTargetAngle / LOWERARM_MOTOR_DEG_PER_TICK);
@@ -137,9 +161,13 @@ public class TempMain extends OpMode {
         // Show the elapsed game time and wheel power.
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motor encoder ticks", "lower (%.2f), upper 1 (%.2f), upper 2 (%.2f)", lowerMotor.getCurrentPosition(), upperMotor1.getCurrentPosition(), upperMotor2.getCurrentPosition());
-        //show target X,Y
         telemetry.addData("Target x,y", "x:(%.2f), y:(%.2f)", targetX, targetY);
+        telemetry.addData("Target angles", "lower:(%.2f), y:(%.2f)", lowerTargetAngle, upperTargetAngle);
+    }
 
+    //law of cosines, solved to find angle C
+    private double lawOfCosines(double a, double b, double c) {
+        return Math.acos((a*a + b*b - c*c) / (2 * a * b));
     }
 
     /*
